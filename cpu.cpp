a74ebc412163842e491a75d8914e7bb5bb07a5ae
TheRealDynamo <dylan.t.brackett@gmail.com>
2015-10-29 14:09:29 -0400

    Intital Commit

--- cpu.cpp
+++ cpu.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdint>
 
-void gr8chip8::emulateCycle(pc) {
+void gr8chip8::emulateCycle(uint8_t pc) {
 
 }
--- gr8chip8.cpp
+++ gr8chip8.cpp
@@ -2,5 +2,5 @@
 #include <cstdint>
 
 int main(int argc, char* argv[]) {
-
+	return 0;
 }
