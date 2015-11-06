50ad25db2f5655c942b64f033b13e91cb128dab8
Dylan Brackett <dbrackett16@pocohi.com>
2015-11-06 14:11:59 -0500

    Initial

---
+++ chip8.cpp
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include "chip8.h"
+
+// Initialize emulator
+void Chip8::init() {
+
+}
+
+// Take an opcode and run it
+void Chip8::emulateCycle() {
+
+}
+
+
--- chip8.h
+++ chip8.h
@@ -1,7 +1,29 @@
-#ifndef GR8CHIP8_H
-#define GR8CHIP8_H
+#ifndef CHIP8_H
+#define CHIP8_H
+
+#define MEMSIZE 4096
+#define REGSIZE 16
+
 class Chip8 {
 public:
     float version = 0.1;
+
+    void emulateCycle();
+
+private:
+    // Stores current opcode
+    uint16_t opcode;
+    // Index register 0x000 to 0xFFF
+    uint16_t iReg;
+    // Program register 0x000 to 0xFFF
+    uint16_t pc;
+    // Chip8 memory
+    uint8_t memory[MEMSIZE];
+    // General registers V0 to VE
+    // 16th register is cary flag
+    uint8_t vReg[REGSIZE];
+
+    void init();
 };
-#endif //GR8CHIP8_H
+#endif //CHIP8_H
+
