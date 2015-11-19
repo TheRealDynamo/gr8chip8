8093260bad182706024bb78cd39c1153e5cad8c7
DB <dylan.t.brackett@gmail.com>
2015-11-19 15:38:13 -0500

    Merge branch 'master' of https://www.github.com/therealdynamo/gr8chip8
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "SDL.h"
 #include "chip8.h"
 
 int main(int argc, char *argv[]) {
@@ -11,5 +12,9 @@ int main(int argc, char *argv[]) {
         }
     }
 */
+    if (SDL_Init(SDL_INIT_EVERYTHING) == -1) {
+        std::cout << "";
+    }
+
     return 0;
 }
