d3a1cd31e111e26ad7cd465da398a7801c1a1337
TheRealDynamo <dylan.t.brackett@gmail.com>
2015-11-20 13:26:15 -0500

    Initial

--- main.cpp
+++ main.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
-#include "SDL.h"
+#include "SDL2/SDL.h"
 #include "chip8.h"
 
 int main(int argc, char *argv[]) {
-   /* Chip8 chip;
 
-    while(1) {
-        chip.emulateCycle();
-        if (chip.getDrawFlag()) {
-
-        }
-    }
-*/
-    if (SDL_Init(SDL_INIT_EVERYTHING) == -1) {
-        std::cout << "";
-    }
-
-    return 0;
 }
